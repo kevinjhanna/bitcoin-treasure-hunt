a5d807858c16ad4bedb2846ad4458128ebe7057e
Kevin Hanna <kevinjackhanna@gmail.com>
2016-11-06 15:40:45 -0300

    Add some basic types & create multiSig tx

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,49 @@ import * as _ from 'lodash'
 import { promisifyAll } from 'bluebird'
 
 declare var require: Function
-const bitcore = require('bitcore-lib')
+const bitcore = require('bitcore-lib') as Bitcore
 const Insight = require('bitcore-explorers').Insight
 const qrcode = require('qrcode-terminal')
+
+// Types
+interface Bitcore {
+  PrivateKey: any
+  Transaction: any
+  Address: any
+  Unit: any
+
+  Networks: {
+    defaultNetwork: any
+    testnet: any
+  }
+}
+
+interface Address {
+  toString: () => string
+}
+
+interface PublicKey {
+}
+
+interface PrivateKey {
+  toAddress: () => Address
+  toPublicKey: () => PublicKey
+}
+
+interface UnspentOutput {
+  outputIndex: number
+  satoshis: number
+}
+
+interface Transaction {
+  from: (utxos: any, publicKeys?: PublicKey[], treshold?: number) => Transaction
+  to: (address: Address, amount: number) => Transaction
+  change: (address: Address) => Transaction
+  fee: (fee: number) => Transaction
+  sign: (privateKeys: PrivateKey | PrivateKey[]) => Transaction
+  serialize: () => string
+}
+
 
 // Use testnet
 bitcore.Networks.defaultNetwork = bitcore.Networks.testnet
@@ -12,6 +52,7 @@ const insight = new Insight('testnet')
 
 const insightPromise = promisifyAll(insight) as {
   getUnspentUtxosAsync: (foo:any) => any
+  broadcastAsync: (transaction: string) => any
 }
 
 function delay(ms: number) {
@@ -32,18 +73,54 @@ async function waitForTransaction(address: any) : Promise<any>{
   }
 }
 
+
+function createPrivateKeys(n: number) {
+  return _.chain(n)
+    .range()
+    .map(_ => new bitcore.PrivateKey() as PrivateKey)
+    .value()
+}
+
+// In satoshis
+function totalAmount(utxos: UnspentOutput[]) : number {
+  return _.chain(utxos)
+    .map(utxo => utxo.satoshis)
+    .sum()
+    .value()
+}
+
 async function run() {
   // Create funding address
-  const privateKey = new bitcore.PrivateKey();
-  const address = privateKey.toAddress();
+  const fundingPrivateKey = new bitcore.PrivateKey() as PrivateKey
+  const fundingAddress = fundingPrivateKey.toAddress()
 
   console.log("Send bitcoin to this address")
-  console.log(address.toString())
-  qrcode.generate(address.toString())
+  console.log(fundingAddress.toString())
+  qrcode.generate(fundingAddress.toString())
 
   try {
-    const utxos = await waitForTransaction(address)
-    console.log("Finally utxos", utxos)
+    const utxos = await waitForTransaction(fundingAddress)
+    console.log("Address funded", utxos)
+
+    const tokens = createPrivateKeys(10)
+    const tokenPublicKeys = tokens.map(token => token.toPublicKey())
+    const requiredTokens = 2
+
+    const prizeAddress = new bitcore.Address(tokenPublicKeys, requiredTokens) as Address
+
+    const fee = 6000
+    const prizeAmount = totalAmount(utxos) - fee
+
+    const transaction = (new bitcore.Transaction() as Transaction)
+      .from(utxos)
+      .fee(fee)
+      .to(prizeAddress, prizeAmount)
+      .sign(fundingPrivateKey)
+
+    console.log("Broadcasting transaction")
+    await insightPromise.broadcastAsync(transaction.serialize())
+    console.log("Transaction broadcasted successfully")
+
   } catch(err) {
     console.error(err)
   }
